a164179dda38e0e04a693d39e64cf0d3af2e68e9
vilgad <rohitgupta16685@gmail.com>
2022-04-18 07:27:40 +0530

    Sponsors section added

--- src/Components/Sponsors/sponsors.js
+++ src/Components/Sponsors/sponsors.js
@@ -1,14 +1,28 @@
 import "./sponsors.css";
 import codersEvoke from "../assets/img/logo.png";
+import gmc from "../assets/img/givemycertificate.png";
+import azure from "../assets/img/microsoft-azure-logo.png";
+import gfg from "../assets/img/gfg-new-logo.png";
 import Border from "../Customized Border/border";
 
 export default function Sponsors() {
   return (
-    <section className="my-5">
+    <section className="my-5 container">
       <div>
         <h2 className="text-center fs-1">Sponsors</h2>
       </div>
       <Border />
+      <div className="sponsor-img d-flex justify-content-center align-items-center">
+        <a href="https://givemycertificate.com/">
+          <img src={gmc} alt="Give my certificate.png" />
+        </a>
+        <a href="https://azure.microsoft.com/en-in/">
+          <img src={azure} alt="Microsoft azure" />
+        </a>
+        <a href="https://www.geeksforgeeks.org/">
+          <img src={gfg} alt="geeksforgeeks" />
+        </a>
+      </div>
     </section>
   );
 }
