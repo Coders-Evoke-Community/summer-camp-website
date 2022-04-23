81779b23050763f9affacd99bfc82ecb39065121
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-04-23 12:06:08 +0530

    faqs section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Hero from "./Components/Hero";
 import About from "./Components/About/about";
 import Sponsors from "./Components/Sponsors/sponsors";
 import Team from "./Components/Team/team";
-import Footer from "./Components/Footer/footer"
+import Faqs from "./Components/Faqs/faqs";
+import Footer from "./Components/Footer/footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Sponsors />
       <Team />
+      <Faqs />
       <Footer />
     </>
   );
--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./about.css";
 import codersEvoke from "../assets/img/About_Image.svg";
 import Border from "../Customized Border/border";
@@ -8,18 +9,19 @@ export default function About() {
   return (
     <section className="my-5 container" id = "about">
       <div>
-        <h2 className="text-center fs-1">About us</h2>
+        <motion.h2 initial={{opacity:0}} animate = {{opacity:1}} transition = {{duration:1}} className="text-center fs-1">About us</motion.h2>
       </div>
       <Border />
       <div className="row m-auto align-items-center">
         <img className="about-img img-fluid  col mx-2" src={codersEvoke} alt="" />
         <div className="about-text m-2 col">
-          <p className="text-primary fs-2">Welcome to</p>
-          <h1 className="text-danger fs-1 ">The Biggest Summer Fest </h1>
-          <h1 className="text-danger fs-1 ">Of Year 2022</h1>
-          <p className="fs-3 my-auto">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis ea voluptates dicta, explicabo pariatur libero fugit laboriosam quam delectus minima doloribus quaerat est totam quae repudiandae quisquam cupiditate eum! Reprehenderit!</p>
+          <p className="text-primary fs-1">Welcome to</p>
+          <h1 className="about-text-head  ">The Biggest Summer Fest </h1>
+          <h1 className="about-text-head  ">Of Year 2022</h1>
+          <p className="fs-6 my-auto">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis ea voluptates dicta, explicabo pariatur libero fugit laboriosam quam delectus minima doloribus quaerat est totam quae repudiandae quisquam cupiditate eum! Reprehenderit!</p>
         </div>
       </div>
+      
     </section>
   );
 }
